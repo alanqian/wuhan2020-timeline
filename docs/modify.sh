8f501d08bfaa36967445ac1140f99506e7df7855
Weilei Zeng <240155787@qq.com>
2020-03-02 22:13:46 -0800

    update *md*

--- docs/modify.sh
+++ docs/modify.sh
@@ -1,8 +1,8 @@
 # you can run this by input 
 
-sed  's/|/\\|/g' 时间线TIMELINE.md > index.md
+sed  's/|/\\|/g' ../时间线TIMELINE.md > index.md
 
-cp readme.md about.md
+cp ../readme.md about.md
 
 
 # sed 's/|/\\|/g' test.txt > timeline.md
