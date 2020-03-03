d60b40262d8d2d581f8363da0cee5e496e5fdf34
Weilei Zeng <240155787@qq.com>
2020-03-03 14:26:42 -0800

    add github action

--- docs/modify.sh
+++ docs/modify.sh
@@ -3,7 +3,8 @@
 #sed  's/|/\\|/g' ../时间线TIMELINE.md > index.md
 
 sed  's/|/·/g' ../时间线TIMELINE.md > index.md
-
+pip3 install -r requirement.txt
+python3 extract_data_to_yaml.py
 
 cp ../readme.md about.md
 
