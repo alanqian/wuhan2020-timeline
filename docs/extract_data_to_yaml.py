f96231aef013da51e3f20f95bc89717fdc3e1a01
Weilei Zeng <240155787@qq.com>
2020-03-02 23:14:50 -0800

    test yaml data

--- docs/extract_data_to_yaml.py
+++ docs/extract_data_to_yaml.py
@@ -1,7 +1,8 @@
 # extract data from index.md to _data/news.yml
 
 
-file_input=open('index.md','r')
+#file_input=open('index.md','r')
+file_input=open('../时间线TIMELINE.md','r')
 file_output=open('_data/news.yml','w')
 
 file_input.readline()
@@ -15,10 +16,10 @@ for line in file_input:
         
         print('date: '+line)
     elif line[0:1] == "【":
-        file_output.write('  group: '+line)
+        file_output.write('  - item_name: group\n    content: "'+line[:-1] + '"\n')
         #print('group '+ line)
     elif line[0] == '*':
-        file_output.write('  - "'+line[2:-1]+'"\n')
+        file_output.write('  - item_name: news\n    content: "'+line[2:-1]+'"\n')
 
 
 file_input.close()
