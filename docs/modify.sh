f7f752ef47516dc5f66f1341b3956375d10f29cd
Radish Meeting <240155787@qq.com>
2020-03-05 18:23:06 -0800

    Update modify.sh

--- docs/modify.sh
+++ docs/modify.sh
@@ -3,11 +3,13 @@
 #sed  's/|/\\|/g' ../时间线TIMELINE.md > index.md
 
 sed  's/|/·/g' ../时间线TIMELINE.md > index.md
+
 #pip3 install -r requirement.txt
 python3 extract_data_to_yaml.py
 
-cp ../README.md about.md
 
+echo "<br><a name="bottom">[回到顶部](#top) </a>" >> index.md
 
 # sed 's/|/\\|/g' test.txt > timeline.md
 # cat timeline.md
+cp ../README.md about.md
